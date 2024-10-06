7aa0a08ad01d47a958d8ddbf3cb473b7b7cdc2b4
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-10-06 23:32:15 +0300

    unauthorized user will get a toast when they try accessing protected routes

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,9 +1,22 @@
 import { Navigate } from "react-router-dom";
 import useAuth from "@/hooks/useAuth";
 import SpinnerIcon from "@/assets/icons/SpinnerIcon";
+import { useToast } from "@/hooks/use-toast";
+import { useEffect } from "react";
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
   const { loading, user } = useAuth();
+  const { toast } = useToast();
+  useEffect(() => {
+    if (!user && !loading) {
+      console.log("first");
+      toast({
+        title: "Please Log In",
+        description: "You need to be logged in to view this page.",
+        variant: "destructive",
+      });
+    }
+  }, [user, loading]);
 
   if (loading) {
     return <SpinnerIcon />;
@@ -13,7 +26,12 @@ const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
     return children;
   }
 
-  return <Navigate replace to='/log-in' />;
+  return (
+    <Navigate
+      replace
+      to='/log-in'
+    />
+  );
 };
 
 export default PrivateRoute;
