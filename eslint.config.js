adcc5f2d53e6cb136ab142ef580937b6ccd55148
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-12-27 09:30:31 +0300

    chors: fix eslint deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,40 @@
-import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import vitestGlobals from "eslint-plugin-vitest-globals";
 import query from "@tanstack/eslint-plugin-query";
-
+import eslint from "@eslint/js";
 export default tseslint.config(
-  { ignores: ["dist", "src/components/ui/*", "tailwind.config.js"] },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: [
+      "dist",
+      "src/components/ui/*",
+      "tailwind.config.js",
+      "playwright.config.ts",
+    ],
+  },
+  // fix the linting with typescript
+  {
     files: ["**/*.{ts,tsx}"],
 
     languageOptions: {
       ecmaVersion: 2020,
+      parser: tseslint.parser,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "vitest-globals": vitestGlobals,
       "@tanstack/react-query": query,
+      "@typescript-eslint": tseslint.plugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
