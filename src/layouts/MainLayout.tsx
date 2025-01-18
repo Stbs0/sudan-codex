b260a94bdca463e55cb57d9acaacdef58d534506
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-01-18 21:41:59 +0300

    style: add animation to the pages

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,17 @@
 import Header from "@/components/MainLayout/Header";
 
 import Footer from "@/components/MainLayout/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import { Toaster } from "sonner";
 
 import { AppSidebar } from "@/components/MainLayout/app-sidebar";
 import useAuth from "@/hooks/useAuth";
 import DevAlert from "@/components/DevAlert";
+import { AnimatePresence, motion } from "motion/react";
 
 const MainLayout = () => {
+  const location = useLocation();
   const { user } = useAuth();
   return (
     <>
@@ -17,10 +19,19 @@ const MainLayout = () => {
       <div className='grid w-full content-between dark:bg-slate-800'>
         <Header />
 
-        <main className='container mx-auto py-8'>
-          <Outlet />
-        </main>
-
+        {/* <main className='container mx-auto py-8'> */}
+        {/* <Outlet /> */}
+        {/* </main> */}
+        <AnimatePresence mode='wait'>
+          <motion.main
+            className='container mx-auto py-8'
+            key={location.pathname}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}>
+            <Outlet />
+          </motion.main>
+        </AnimatePresence>
         <Toaster
           richColors
           expand={true}
