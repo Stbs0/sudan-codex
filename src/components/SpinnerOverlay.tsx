bc96f1ae546fa03e6f4ee1c4ca0f1667fc44f16b
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-01-19 12:45:04 +0300

    refractor: moved spinner overlay into the body

--- src/components/SpinnerOverlay.tsx
+++ src/components/SpinnerOverlay.tsx
@@ -1,13 +1,19 @@
-import Spinner from "../assets/icons/SpinnerIcon.svg";
+import { LoaderCircle } from "lucide-react";
+import { motion } from "motion/react";
+import ReactDOM from "react-dom";
 
 const SpinnerOverlay = () => {
-  return (
-    <div className='absolute left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-gray-800/80'>
-      <img
-        className='w-20 animate-spin'
-        src={Spinner}
+  const LoadingSpinner = motion.create(LoaderCircle);
+
+  return ReactDOM.createPortal(
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-gray-800/50'>
+      <LoadingSpinner
+        animate={{ rotate: 360 }}
+        transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
+        className='h-16 w-16 text-purple-600'
       />
-    </div>
+    </div>,
+    document.body // Render the spinner at the end of the body element
   );
 };
 
