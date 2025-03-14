05d3ab39e558a0abdede0823d5def2077a04a04a
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-03-14 07:20:40 +0300

    fix: prevent unnecessary navigation in Logo component when on home page

--- src/components/MainLayout/Logo.tsx
+++ src/components/MainLayout/Logo.tsx
@@ -1,13 +1,22 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Button } from "../ui/button";
 import LogoPic from "../../assets/logo/purpleLogo.png";
+
 const Logo = ({ className }: { className?: string }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleClick = () => {
+    if (location.pathname !== "/") {
+      navigate("/", {});
+    }
+  };
+
   return (
     <Button
       className={`h-full p-0 hover:bg-transparent ${className}`}
       variant={"ghost"}
-      onClick={() => navigate("/")}>
+      onClick={handleClick}>
       <img
         className='h-full w-full object-contain'
         src={LogoPic}
