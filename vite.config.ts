79ebd37c8b401ac566ec399efc8d95ca738f07d8
Mohammed Mahmoud <mohammedjrt@gmail.com>
2025-08-06 20:22:04 +0300

    feat add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   test: {
@@ -13,10 +14,36 @@ export default defineConfig({
     exclude: ["react-scan"],
   },
   envDir: "./envDir",
-  plugins: [react(), tailwindcss()],
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.icon", "logo192.png", "logo512.png"],
+      manifest: {
+        name: "Sudan Codex",
+        short_name: "SudanCodex",
+        description:
+          "Sudan Codex allows you to effortlessly search through Sudan's comprehensive drug index. Find drug information, generic names, and manufacturers quickly and accurately.",
+        display: "standalone",
+        icons: [
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   preview: {
     port: +(process.env.VITE_PREVIEW_PORT || 4173),
   },
