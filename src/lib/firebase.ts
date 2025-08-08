c433ed1f8c86012a539c5333fa8d58db7c3ee571
Mohammed Mahmoud <mohammedjrt@gmail.com>
2025-08-08 14:48:52 +0300

    fix: firebase config typo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,11 @@
-import { initializeApp } from "firebase/app";
+import { FirebaseOptions, initializeApp } from "firebase/app";
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderI: import.meta.env.VITE_MESSAGE_SENDER,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER,
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
