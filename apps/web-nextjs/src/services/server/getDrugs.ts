57a7964912738c559ea42b7895843b1d1774459f
Mohammed Ibrahim <mohammedjrt@gmail.com>
2025-12-03 00:22:04 +0300

    feat: fetch drugs by no with tursodb

--- apps/web-nextjs/src/services/server/getDrugs.ts
+++ apps/web-nextjs/src/services/server/getDrugs.ts
@@ -1,4 +1,5 @@
-import { Drug } from "@/lib/types";
+import { client } from "@/lib/tursoDB";
+import { Drug, DrugWithSlugs } from "@/lib/types";
 import fs from "fs/promises";
 import path from "path";
 import { cache } from "react";
@@ -8,10 +9,15 @@ export const getAllDrugs = cache(async (): Promise<Drug[]> => {
   return JSON.parse(await fs.readFile(filePath, "utf8"));
 });
 
-export const getDrugByNo = cache(async (no: string): Promise<Drug | null> => {
-  // You can even reuse the first function to keep caching efficient
-  const data = await getAllDrugs();
-  const drug = data.find((d) => d.no === no) ?? null;
+export const getDrugByNo = cache(
+  async (no: string): Promise<DrugWithSlugs | null> => {
+    // You can even reuse the first function to keep caching efficient
+    const data = await client.execute({
+      sql: "SELECT * FROM drugs WHERE no = ?",
+      args: [no],
+    });
+    if (data.rows.length === 0) return null;
 
-  return drug;
-});
+    return data.rows[0] as unknown as DrugWithSlugs;
+  }
+);
