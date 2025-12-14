527aca81600ed403eed00477471b5d6b7b277274
Mohammed Ibrahim <mohammedjrt@gmail.com>
2025-12-14 14:37:00 +0300

    feat: add api routes for getting the agents, generics and companies

---
+++ apps/web-nextjs/src/app/api/generics/[slug]/route.ts
@@ -0,0 +1,62 @@
+import db from "@/db";
+import { drugsTable, genericsTable } from "@/db/schemas/schema";
+import { eq } from "drizzle-orm";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  ctx: RouteContext<"/api/generics/[slug]">,
+) {
+  const { slug } = await ctx.params;
+  if (!slug) {
+    return Response.json({ error: "No slug provided" }, { status: 400 });
+  }
+
+  const generic = await db.query.genericsTable.findFirst({
+    where: eq(genericsTable.slug, slug),
+    with: {
+      stats: {
+        columns: {
+          total_brands: true,
+          related_agents: true,
+          related_companies: true,
+        },
+      },
+    },
+  });
+
+  if (!generic)
+    return Response.json({ error: "Drug not found" }, { status: 404 });
+  const drugsWithAll = await db.query.drugsTable.findMany({
+    where: eq(drugsTable.generic_id, generic.id),
+    columns: {
+      brand_name: true,
+      pack_size: true,
+      slug: true,
+      company_name: true,
+      generic_name: true,
+      strength: true,
+    },
+    with: {
+      agent: {
+        columns: {
+          slug: true,
+        },
+      },
+      company: {
+        columns: {
+          slug: true,
+        },
+      },
+    },
+  });
+
+  return Response.json({
+    stats: {
+      total_brands: generic.stats.total_brands,
+      related_agents: generic.stats.related_agents,
+      related_companies: generic.stats.related_companies,
+    },
+    drugs: drugsWithAll,
+  });
+}
