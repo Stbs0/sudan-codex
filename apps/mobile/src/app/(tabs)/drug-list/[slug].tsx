f59c4101e954e9e94ce965a20ea220497a63f4e8
Mohammed Ibrahim <mohammedjrt@gmail.com>
2025-12-15 21:56:17 +0300

    fix: one hellof a bug

--- apps/mobile/src/app/(tabs)/drug-list/[slug].tsx
+++ apps/mobile/src/app/(tabs)/drug-list/[slug].tsx
@@ -12,9 +12,6 @@ import { Stack, useLocalSearchParams, useNavigation } from "expo-router";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { ActivityIndicator, ScrollView, View } from "react-native";
-import RenderHtml, {
-  type MixedStyleDeclaration,
-} from "react-native-render-html";
 const DrugInfo = () => {
   const { t } = useTranslation();
   const { slug } = useLocalSearchParams<{ slug: string }>();
@@ -241,45 +238,45 @@ const DrugInfo = () => {
 };
 export default DrugInfo;
 
-const tagsStyles: Readonly<Record<string, MixedStyleDeclaration>> = {
-  b: {
-    fontStyle: "normal",
-    fontWeight: "bold",
-  },
-  ul: { marginVertical: 8, paddingLeft: 20 },
-  li: { marginBottom: 4 },
-} as const;
+// const tagsStyles: Readonly<Record<string, MixedStyleDeclaration>> = {
+//   b: {
+//     fontStyle: "normal",
+//     fontWeight: "bold",
+//   },
+//   ul: { marginVertical: 8, paddingLeft: 20 },
+//   li: { marginBottom: 4 },
+// } as const;
 
-const DrugAccordion = ({
-  trigger,
-  content,
-  width,
-  colorSchema,
-}: {
-  trigger: string;
-  content: string;
-  width: number;
-  colorSchema: "light" | "dark" | undefined;
-}) => {
-  const { t } = useTranslation();
-  const html = content || `<p><i>${t("drugInfo.noDataAvailable")}</i></p>`;
-  return (
-    <AccordionItem key={trigger} value={trigger}>
-      <AccordionTrigger>
-        <Text>{trigger.toUpperCase()}</Text>
-      </AccordionTrigger>
-      <AccordionContent className="">
-        <RenderHtml
-          tagsStyles={tagsStyles}
-          enableExperimentalBRCollapsing={true}
-          contentWidth={width}
-          source={{ html }}
-          // defaultTextProps={{
-          //   style: { color: "white" },
-          // }}
-          baseStyle={{ color: colorSchema === "dark" ? "white" : "black" }}
-        />
-      </AccordionContent>
-    </AccordionItem>
-  );
-};
+// const DrugAccordion = ({
+//   trigger,
+//   content,
+//   width,
+//   colorSchema,
+// }: {
+//   trigger: string;
+//   content: string;
+//   width: number;
+//   colorSchema: "light" | "dark" | undefined;
+// }) => {
+//   const { t } = useTranslation();
+//   const html = content || `<p><i>${t("drugInfo.noDataAvailable")}</i></p>`;
+//   return (
+//     <AccordionItem key={trigger} value={trigger}>
+//       <AccordionTrigger>
+//         <Text>{trigger.toUpperCase()}</Text>
+//       </AccordionTrigger>
+//       <AccordionContent className="">
+//         <RenderHtml
+//           tagsStyles={tagsStyles}
+//           enableExperimentalBRCollapsing={true}
+//           contentWidth={width}
+//           source={{ html }}
+//           // defaultTextProps={{
+//           //   style: { color: "white" },
+//           // }}
+//           baseStyle={{ color: colorSchema === "dark" ? "white" : "black" }}
+//         />
+//       </AccordionContent>
+//     </AccordionItem>
+//   );
+// };
