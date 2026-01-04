512f28863d0f2d1fe57b7c382f9d24e02f52ca7c
Mohammed Ibrahim <mohammedjrt@gmail.com>
2026-01-04 18:24:12 +0300

    chore: add meticoluse

--- apps/web-nextjs/src/app/layout.tsx
+++ apps/web-nextjs/src/app/layout.tsx
@@ -76,6 +76,17 @@ export default function RootLayout({
           href='/favicon.ico'
           type='image/x-icon'
         />
+        {(process.env.NODE_ENV === "development" ||
+          process.env.VERCEL_ENV === "preview") && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-recording-token={
+              process.env.NEXT_PUBLIC_METICULOUS_RECORDING_TOKEN
+            }
+            data-is-production-environment='false'
+            src='https://snippet.meticulous.ai/v1/meticulous.js'
+          />
+        )}
       </head>
       <body
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
