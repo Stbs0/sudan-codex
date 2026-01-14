40c282c463f0fa1052f01a1218986a8e6ff4bb03
Mohammed Ibrahim <mohammedjrt@gmail.com>
2026-01-15 01:22:52 +0300

    clean app the project

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -4,7 +4,7 @@ import "tsx/cjs";
 const IS_DEV = process.env.APP_VARIANT === "development";
 const IS_PREVIEW = process.env.APP_VARIANT === "preview";
 
-const getPackageIdentifier = () => {
+const getBundleIdentifier = () => {
   if (IS_DEV) {
     return "app.sudancodex.mobile.dev";
   }
@@ -14,6 +14,17 @@ const getPackageIdentifier = () => {
   }
 
   return "app.sudancodex.mobile";
+};
+const getPackageIdentifier = () => {
+  if (IS_DEV) {
+    return "com.sudancodex.mobile.dev";
+  }
+
+  if (IS_PREVIEW) {
+    return "com.sudancodex.mobile.preview";
+  }
+
+  return "com.sudancodex.mobile";
 };
 const getAppName = () => {
   if (IS_DEV) {
@@ -41,86 +52,18 @@ const getScheme = () => {
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: getAppName(),
   slug: "sudancodex",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./src/assets/icons/adaptive-icon.png",
+
+  name: getAppName(),
   scheme: getScheme(),
-  userInterfaceStyle: "automatic",
-  newArchEnabled: true,
-  splash: {
-    image: "./src/assets/icons/splash-icon-light.png",
-    resizeMode: "contain",
-    backgroundColor: "#232323",
-  },
+
   ios: {
-    supportsTablet: true,
-    bundleIdentifier: getPackageIdentifier(),
+    ...config.ios,
+    bundleIdentifier: getBundleIdentifier(),
   },
   android: {
-    adaptiveIcon: {
-      foregroundImage: "./src/assets/icons/adaptive-icon.png",
-      backgroundColor: "#ffffff",
-    },
-    edgeToEdgeEnabled: true,
-    softwareKeyboardLayoutMode: "resize",
-    package: getPackageIdentifier(),
-  },
+    ...config.android,
 
-  web: {
-    bundler: "metro",
-    favicon: "",
-  },
-  plugins: [
-    ["./plugins/withNewHermsPath.ts"],
-    "expo-router",
-    [
-      "expo-localization",
-      {
-        supportedLocales: {
-          android: ["en", "ar"],
-          ios: ["en", "ar"],
-        },
-      },
-    ],
-    ["expo-sqlite"],
-    [
-      "expo-build-properties",
-      {
-        android: {
-          compileSdkVersion: 35,
-          targetSdkVersion: 35,
-          minSdkVersion: 28,
-        },
-      },
-    ],
-    [
-      "expo-splash-screen",
-      {
-        backgroundColor: "#232323",
-        image: "./src/assets/icons/splash-icon-light.png",
-        dark: {
-          image: "./src/assets/icons/splash-icon-dark.png",
-          backgroundColor: "#000000",
-        },
-        imageWidth: 200,
-      },
-    ],
-    "expo-font",
-  ],
-  experiments: {
-    typedRoutes: true,
-    reactCompiler: true,
-    autolinkingModuleResolution: true,
-  },
-  owner: "stbs0",
-  extra: {
-    router: {},
-    supportsRTL: true,
-    eas: {
-      projectId: "df521420-8fc5-4677-83b6-54c8171edc1e",
-    },
-    apiUrl: process.env.EXPO_PUBLIC_BACKEND_URI,
+    package: getPackageIdentifier(),
   },
 });
